050bc9cd9efcaafe44023ac053267b8171e9321c
abdullrahman <aljizzani.abdullrahman@gmail.com>
2023-07-03 18:26:24 +0300

    fix certifiForm import

--- src/app/dashboard/certificate/newCertificate/page.jsx
+++ src/app/dashboard/certificate/newCertificate/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import CertifiForm from "./components/certifiForm";
+import CertifiForm from "../components/certifiForm";
 export default function newCertificate() {
   return (
     <div>
