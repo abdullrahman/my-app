bb38c3dbe932529a9d9ca7952bff8899cf8d3cec
abdullrahman <aljizzani.abdullrahman@gmail.com>
2023-07-03 20:24:15 +0300

    disable user page

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -3,20 +3,19 @@
 import { PrismaClient } from "@prisma/client";
 export default async function User() {
   // const { data: session } = useSession();
-  const prisma = new PrismaClient();
-
-  const getData = async () => {
-    const data = await prisma.info.update({
-      data: {
-        password: "123456",
-      },
-      where: {
-        id: 1,
-      },
-    });
-    return data;
-  };
-  const data = await getData();
-  console.log(data);
-  return <pre>{JSON.stringify(data)}</pre>;
+  // const prisma = new PrismaClient();
+  // const getData = async () => {
+  //   const data = await prisma.info.update({
+  //     data: {
+  //       password: "123456",
+  //     },
+  //     where: {
+  //       id: 1,
+  //     },
+  //   });
+  //   return data;
+  // };
+  // const data = await getData();
+  // console.log(data);
+  // return <pre>{JSON.stringify(data)}</pre>;
 }
