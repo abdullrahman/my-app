c0b22b171ffaed566676ade8d845d54da468433a
abdullrahman <aljizzani.abdullrahman@gmail.com>
2023-07-03 18:31:58 +0300

    fix user route page

--- src/app/user/api/route.jsx
+++ src/app/user/api/route.jsx
@@ -4,23 +4,23 @@ const prisma = new PrismaClient();
 
 export async function GET(req) {
   const info = await prisma.info.findMany({
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      // password: true,
-      careerObJ: true,
-      summary: true,
-      city: true,
-      role: true,
-      onDelete: true,
-    },
-    where: {
-      id: 1,
-    },
-    include: {
-      certificate: true,
-    },
+    // select: {
+    //   id: true,
+    //   name: true,
+    //   email: true,
+    //   // password: true,
+    //   careerObJ: true,
+    //   summary: true,
+    //   city: true,
+    //   role: true,
+    //   onDelete: true,
+    // },
+    // where: {
+    //   id: 1,
+    // },
+    // include: {
+    //   certificate: true,
+    // },
   });
   return new Response(JSON.stringify({ info }));
 }
